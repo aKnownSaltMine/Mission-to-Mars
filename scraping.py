9f21e3930ac58f37ba8b8f91522f239b4e66233c
aKnownSaltMine <ctcarpenter72@yahoo.com>
2022-04-24 19:30:00 -0500

    final submission

--- scraping.py
+++ scraping.py
@@ -19,6 +19,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        'hemispheres': mars_hemispheres(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -96,6 +97,29 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def mars_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(4):
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.links.find_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css('h2.title').text
+        hemispheres['title'] = title
+        hemispheres["img_url"] = img_url
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
